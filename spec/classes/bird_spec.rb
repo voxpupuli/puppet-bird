eb1ce29741b413d87f0099e74d2b7bf880b94b2b
Sebastien Badia <seb@sebian.fr>
2013-10-27 16:54:07 +0100

    improve spec file

--- spec/classes/bird_spec.rb
+++ spec/classes/bird_spec.rb
@@ -13,8 +13,93 @@ describe 'bird', :type => :class do
 
   context "On Debian" do
     let(:facts) {{ :osfamily => 'Debian' }}
-    it { should include_class("bird::params") }
-    it { should contain_package('bird') }
-  end
 
+    context "with only IPv4" do
+      let(:params) {{
+        :config_file_v4 => 'puppet:///modules/fooboozoo',
+        :enable_v6      => false,
+      }}
+      it { should include_class("bird::params") }
+      it { should contain_package('bird') }
+      it { should contain_service('bird').with(
+        :ensure     => 'running',
+        :enable     => 'true',
+        :hasstatus  => 'false',
+        :pattern    => 'bird',
+        :hasrestart => 'false',
+        :restart    => '/usr/sbin/birdc configure'
+      )}
+
+      it { should contain_file('/etc/bird.conf').with(
+        :source => 'puppet:///modules/fooboozoo',
+        :owner  => 'root',
+        :group  => 'root',
+        :mode   => '0644',
+        :notify => 'Service[bird]',
+      )}
+    end
+
+    context "with IPv4 and IPv6" do
+      let(:params) {{
+        :config_file_v4 => 'puppet:///modules/fooboozoo',
+        :enable_v6      => true,
+        :config_file_v6 => 'puppet:///modules/fooboozoo6',
+      }}
+      it { should include_class("bird::params") }
+      it { should contain_package('bird') }
+      it { should contain_service('bird').with(
+        :ensure     => 'running',
+        :enable     => 'true',
+        :hasstatus  => 'false',
+        :pattern    => 'bird',
+        :hasrestart => 'false',
+        :restart    => '/usr/sbin/birdc configure'
+      )}
+
+      it { should contain_file('/etc/bird.conf').with(
+        :source => 'puppet:///modules/fooboozoo',
+        :owner  => 'root',
+        :group  => 'root',
+        :mode   => '0644',
+        :notify => 'Service[bird]',
+      )}
+
+      it { should contain_package('bird6') }
+      it { should contain_service('bird6').with(
+        :ensure     => 'running',
+        :enable     => 'true',
+        :hasstatus  => 'false',
+        :pattern    => 'bird6',
+        :hasrestart => 'false',
+        :restart    => '/usr/sbin/birdc6 configure'
+      )}
+
+      it { should contain_file('/etc/bird6.conf').with(
+        :source => 'puppet:///modules/fooboozoo6',
+        :owner  => 'root',
+        :group  => 'root',
+        :mode   => '0644',
+        :notify => 'Service[bird6]',
+      )}
+    end
+
+    context "with only IPv4 but config. file unset" do
+      let(:params) {{ :enable_v6 => 'false' }}
+      it "should fail" do
+        expect {
+          should include_class("bird::params")
+        }.to raise_error(Puppet::Error, /must be set/)
+      end
+    end
+
+    context "with IPv4 and IPVv6 but config. file unset" do
+      let(:params) {{ :enable_v6 => 'true' }}
+      it "should fail" do
+        expect {
+          should include_class("bird::params")
+        }.to raise_error(Puppet::Error, /must be set/)
+      end
+    end
+
+  end
 end
