a6d39647210d77ce7cd7a04ec9e5e62ad121a607
Leif <golgy+github@golgy.net>
2015-02-17 11:02:14 +1100

    Added conditional for RedHat based distributions, and added $package_name_v* for removing duplicated resources

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,15 @@ class bird::params {
     'Debian': {
       $daemon_name_v4   = 'bird'
       $daemon_name_v6   = 'bird6'
+      $package_name_v4  = 'bird'
+      $package_name_v6  = 'bird6'
     }
+    'RedHat': {
+      $daemon_name_v4   = 'bird'
+      $daemon_name_v6   = 'bird'
+      $package_name_v4  = 'bird'
+      $package_name_v6  = 'bird'
+    }    
     default: {
       fail("Unsupported osfamily: ${::osfamily}")
     }
