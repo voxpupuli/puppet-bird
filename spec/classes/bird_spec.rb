746f84191be19c08b6fe99dbb0a9b13364cde01e
Sebastien Badia <seb@sebian.fr>
2015-03-18 20:52:15 +0100

    spec: Fix un-supported rspec keyword

--- spec/classes/bird_spec.rb
+++ spec/classes/bird_spec.rb
@@ -4,11 +4,7 @@ describe 'bird', :type => :class do
 
   describe 'On an unknown operating system' do
     let(:facts) {{ :osfamily => 'Unknown' }}
-    it "should fail" do
-      expect do
-        subject
-      end.to raise_error(Puppet::Error, /Unsupported osfamily/)
-    end
+    it { expect { catalogue }.to raise_error(Puppet::Error, /Unsupported osfamily/) }
   end
 
   context "On Debian" do
