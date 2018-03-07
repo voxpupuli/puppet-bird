51590740576840261c24db00e2eaf9bc5492f73f
Pavel Mateja <pavel@verotel.cz>
2018-03-07 12:54:35 +0100

    There is no extra package bird6 in Debian 9 / Ubuntu 16.04
    Use variables defined in params.pp correctly.
    Update default package name for Debian 9.
    
    Co-authored-by: Sebastien Badia <seb@sebian.fr>

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,14 +1,21 @@
 # Class:: bird::params
 #
 class bird::params {
-  case $::osfamily {
+  case $facts['os']['family'] {
     'Debian': {
       $config_path_v4   = '/etc/bird/bird.conf'
       $config_path_v6   = '/etc/bird/bird6.conf'
       $daemon_name_v4   = 'bird'
       $daemon_name_v6   = 'bird6'
       $package_name_v4  = 'bird'
-      $package_name_v6  = 'bird6'
+      # Compatibility for Debian oldstable and Ubuntu old LTS
+      # the bird package now provides bird and bird6 daemons
+      if ($facts['os']['name'] == 'ubuntu' and $facts['lsbdistcodename'] in ['precise', 'trusty']) or
+      ($facts['os']['name'] == 'debian' and $facts['os']['release']['major'] in ['7', '8']) {
+        $package_name_v6  = 'bird6'
+      } else {
+        $package_name_v6  = 'bird'
+      }
     }
     'RedHat': {
       $config_path_v4   = '/etc/bird.conf'
@@ -19,7 +26,7 @@ class bird::params {
       $package_name_v6  = 'bird6'
     }
     default: {
-      fail("Unsupported osfamily: ${::osfamily}")
+      fail("Unsupported osfamily: ${facts['os']['family']}")
     }
   } # Case $::operatingsystem
 } # Class:: bird::params
