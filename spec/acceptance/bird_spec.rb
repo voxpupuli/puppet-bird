311e15aaca4bac3950f33d90c95bc80faf60bd38
Sebastien Badia <seb@sebian.fr>
2018-04-03 16:30:49 +0200

    spec: Added basic acceptance tests

---
+++ spec/acceptance/bird_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper_acceptance'
+
+describe 'bird class' do
+  context 'default parameters' do
+    # Using puppet_apply as a helper
+    it 'works idempotently with no errors' do
+      pp = <<-EOS
+      class { '::bird': }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe package('bird') do
+      it { is_expected.to be_installed }
+    end
+
+    describe service('bird') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+  end
+end
---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,13 @@
+require 'beaker-rspec/spec_helper'
+require 'beaker-rspec/helpers/serverspec'
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
+
+run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
+install_module
+install_module_dependencies
+
+RSpec.configure do |c|
+  # Readable test descriptions
+  c.formatter = :documentation
+end
