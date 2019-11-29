5377cdfd7df5b9a03575af0823623b81d89a064a
Tim Meusel <tim@bastelfreak.de>
2019-11-29 16:25:41 +0100

    puppet-lint: fix legacy_facts

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,7 +10,7 @@ class bird::params {
       $package_name_v4  = 'bird'
       # Compatibility for Debian oldstable and Ubuntu old LTS
       # the bird package now provides bird and bird6 daemons
-      if ($facts['os']['name'] == 'ubuntu' and $facts['lsbdistcodename'] in ['precise', 'trusty']) or
+      if ($facts['os']['name'] == 'ubuntu' and $facts['os']['distro']['release'] in ['precise', 'trusty']) or
       ($facts['os']['name'] == 'debian' and $facts['os']['release']['major'] in ['7', '8']) {
         $package_name_v6  = 'bird6'
       } else {
