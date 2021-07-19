023b9e8cb73cad1b7bf0dbdd5831a420f12873ab
Tim Meusel <tim@bastelfreak.de>
2021-07-19 12:09:47 +0200

    Update spec/defines/snippet_spec.rb
    Co-authored-by: Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>

--- spec/defines/snippet_spec.rb
+++ spec/defines/snippet_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'bird::snippet', type: :define do
+describe 'bird::snippet' do
   on_supported_os.each do |os, facts|
     context "on #{os} " do
       let :facts do
